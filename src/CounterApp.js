c162424ed58a904cfd9db7644e7ef8e392c70f7e
contacto110 <contacto110@gruposof.com>
2020-07-07 00:29:06 -0500

    Contador y useState

---
+++ src/CounterApp.js
@@ -0,0 +1,33 @@
+import React, { Fragment, useState } from 'react';
+import PropTypes from 'prop-types';
+
+
+const CounterApp = ({ value = 1 }) => {
+
+	const [counter, setCounter] = useState(0);
+
+	const handlePlus = () => {
+		// Forma tradicional
+		setCounter(counter + value);
+		// Forma por si no se tiene el state
+		// setCounter(( c )=> c + value);
+	}
+	const handleMinus = () => setCounter(counter - value);
+	const handleClear = () => setCounter(value);
+
+	return (
+		<Fragment>
+			<h1>CounterApp</h1>
+			<h2>{ counter }</h2>
+			<button onClick={ handlePlus }>+{ value }</button>
+			<button onClick={ handleClear }>Clear</button>
+			<button onClick={ handleMinus }>-{ value }</button>
+		</Fragment>
+	);
+}
+
+CounterApp.propTypes = {
+	value: PropTypes.number
+}
+
+export default CounterApp;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PrimeraApp from './PrimeraApp';
+import CounterApp from './CounterApp';
 import './index.css';
 const divRoot =  document.getElementById('root');
 
-ReactDOM.render(<PrimeraApp saludo = 'Hola mundo'/>, divRoot);
+ReactDOM.render(<CounterApp value = {2}/>, divRoot);
