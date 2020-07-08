cf266933f91a3418b99694480d9fe80bdca164eb
contacto110 <contacto110@gruposof.com>
2020-07-07 22:45:04 -0500

    Jest: describe, expect y toBe

--- src/tests/demo.test.js
+++ src/tests/demo.test.js
@@ -1,6 +1,12 @@
-test('Primera prueba', () => {
-	const isActive = false;
-	if (!isActive) {
-		throw new Error('La variable no esta activa');
-	}
+describe('Pruebas en el archivo demo.test.js',() => {
+	test('Los strings deben ser iguales', () => {
+		// 1. Inicializacion
+		const mensaje = 'Hola mundo';
+
+		// 2. Estimulo
+		const mensaje2 = `Hola mundo!`;
+
+		// 3. Observar el comportamiento
+		expect(mensaje).toBe(mensaje2);
+	});
 });
