42b25b9981356de4026a8d4e140502a18e3649af
contacto110 <contacto110@gruposof.com>
2020-07-09 00:59:50 -0500

    Se validan elementos del componente

--- src/tests/PrimeraApp.test.js
+++ src/tests/PrimeraApp.test.js
@@ -9,4 +9,19 @@ describe('Pruebas del componente "PrimeraApp"', () => {
 		const wrapper = shallow(<PrimeraApp saludo = { saludo }/>);
 		expect( wrapper ).toMatchSnapshot();
 	});
+
+	test('Componente debe contener una prop "subtitulo"', () => {
+		const saludo = 'Hola MrMagma!!';
+		const subtitulo = 'Soy un subtitulo';
+		const wrapper = shallow(
+			<PrimeraApp
+				saludo = { saludo }
+				subtitulo = { subtitulo }
+			/>
+		);
+
+		// El metodo funciona igual a document.querySelector();
+		const textoParrafo = wrapper.find('p').text();
+		expect( textoParrafo ).toBe( subtitulo );
+	})
 });
