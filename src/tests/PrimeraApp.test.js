48128e8c54f39cab638ba4e623ec9323d254fa7a
contacto110 <contacto110@gruposof.com>
2020-07-09 00:51:57 -0500

    Se agrega Enzyme

--- src/tests/PrimeraApp.test.js
+++ src/tests/PrimeraApp.test.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
+
 import PrimeraApp from '../PrimeraApp';
 
 describe('Pruebas del componente "PrimeraApp"', () => {
-	test('Debe mostrar un saludo', () => {
-		const saludo = 'Hola soy MrMagma';
-		// const wrapper = render(<PrimeraApp saludo={saludo}/>);
-		// const getByText = wrapper.getByText(saludo);
-		// expect( getByText ).toBeInTheDocument();
-
-		// o se puede desestructurar
-		const { getByText } = render(<PrimeraApp saludo={saludo}/>);
-		expect( getByText(saludo) ).toBeInTheDocument();
+	test('Debe mostrar el componente <PrimeraApp/> correctamente', () => {
+		const saludo = 'Hola MrMagma!!';
+		const wrapper = shallow(<PrimeraApp saludo = { saludo }/>);
+		expect( wrapper ).toMatchSnapshot();
 	});
 });
